be715d79e95f4bd2d26916a96360ed96286980a3
ridwankechil <70096802+ridwankechil@users.noreply.github.com>
2020-10-20 15:58:23 +0700

    Update spam.sh

--- spam.sh
+++ spam.sh
@@ -2,12 +2,15 @@
 echo "selamat datang"
 termux-open https://wa.me/+6285731928045?text=kak+izin+gunain+script+spam+
 echo "masukkan kode scriptnya"
-sleep 2
+sleep 5
 echo "kode scriptnya {567686}"
 read -p "root@Kode Script > " bro
 if [ $bro = 567686 ] || [ $bro = 567686 ]
 then
 sleep 2
+echo "subscribe dulu"
+termux-open https://www.youtube.com/channel/UCTMBlJoMW9jaWyiv8oZsjKw
+sleep 3
 clear
 python Subscribe
 fi

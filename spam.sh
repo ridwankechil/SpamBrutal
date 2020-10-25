877b17b71bf9937648374d46d8ee134eef674a8d
ridwankechil <70096802+ridwankechil@users.noreply.github.com>
2020-10-25 23:39:37 +0700

    Update spam.sh

--- spam.sh
+++ spam.sh
@@ -3,14 +3,14 @@ echo "selamat datang"
 termux-open https://wa.me/+6285731928045?text=kak+izin+gunain+script+spam+
 echo "masukkan kode scriptnya"
 sleep 5
-echo "kode scriptnya {567686}"
+echo "kode scriptnya {W32W58}"
 read -p "root@Kode Script > " bro
-if [ $bro = 567686 ] || [ $bro = 567686 ]
+if [ $bro =W32W58] || [ $bro =W32W58]
 then
 sleep 1
 echo "subscribe dulu"
 sleep 2
-termux-open https://www.youtube.com/channel/UCTMBlJoMW9jaWyiv8oZsjKw
+echo "Subscribe channel ini ya gan"
 sleep 3
 clear
 python Subscribe

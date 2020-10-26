2fec9f0bd554ee29d0fa79ce26daf7219213bd08
ridwankechil <70096802+ridwankechil@users.noreply.github.com>
2020-10-26 08:25:20 +0700

    Update and rename s to spam.sh

--- spam.sh
+++ spam.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 echo "selamat datang"
-termux-open https://wa.me/+6285731928045?text=kak+izin+gunain+script+spam+
+
 echo "masukkan kode scriptnya"
 sleep 5
 echo "kode scriptnya {W32W58}"
 read -p "root@Kode Script > " bro
 if [ $bro =W32W58] || [ $bro =W32W58]
 then
-
+echo "Selamat Mechoba"
 echo "subscribe dulu"
 
 echo "Subscribe channel ini ya gan"
